1a4e11ef84e70e84fdde1a8e3e91165fe3e35df5
Ricky Cheng <scheng32@illinois.edu>
2023-05-18 14:50:07 -0700

    added improvements for more flexibility in testing

--- project1.py
+++ project1.py
@@ -3,6 +3,7 @@ import argparse
 import time
 import random
 import queue
+import csv
 import psycopg2 as pg2
 import multiprocessing as mp
 from dotenv import load_dotenv
@@ -73,7 +74,7 @@ def workerIdea3(transactions, result_q, iso_level, barrier, start_time, id):
             result_q.put_nowait(response)
             # results["Response Time"].append(response)
     #print(f"Process {id} Ended")
-    #result_q.put_nowait(-1)
+    result_q.put_nowait(-1)
     conn.close()
 
 def getConnection():
@@ -115,19 +116,33 @@ def parseQueryFile(fname):
         data_time.append((inst_timestamp, inst_time, inst))
     return data_time
     
-def preprocessWorkload(qfname, ofname, sfname, transaction_size):
+def preprocessWorkload(qfname, ofname, sfname, pfname):
     """
         Preprocess the workload. Output is already batched into transactions of size N
     """
     start = time.time()
-    qry_data = parseQueryFile(qfname)
-    obs_data = parseInsertFile(ofname)
-    sem_data = parseInsertFile(sfname)
+    try:
+        with open(pfname, 'r') as f:
+            workload_data = [tuple([float(line[0]), line[1], line[2]]) for line in csv.reader(f)]
+    except FileNotFoundError:
+        print("No file found. Preprocessing data.")
+        qry_data = parseQueryFile(qfname)
+        obs_data = parseInsertFile(ofname)
+        sem_data = parseInsertFile(sfname)
 
-    workload_data = qry_data + obs_data + sem_data
-    random.shuffle(workload_data)
-    workload_data = sorted(workload_data, key=lambda x: x[0])
+        workload_data = qry_data + obs_data + sem_data
+        random.shuffle(workload_data)
+        workload_data = sorted(workload_data, key=lambda x: x[0])
+        with open(pfname, 'w') as out:
+            csv_out=csv.writer(out)
+            for row in workload_data:
+                csv_out.writerow(row)
+        print(f"Saved preprocessed data into \'{pfname}\'")
 
+    print(f"Finished preprocessing in {time.time() - start:.2f}")
+    return workload_data
+
+def prepareTransactions(workload_data, transaction_size):
     # rescale each day into 60 seconds
     # 24 minutes in original time = 1 second in new timescale
     RESCALE_FACTOR = 60*24 
@@ -143,30 +158,25 @@ def preprocessWorkload(qfname, ofname, sfname, transaction_size):
             workload_inst += unit[2] + " "
         
         workload_transactions.append(tuple([new_timestamp, workload_batch[-1][1], workload_inst]))
-    random.shuffle(qry_data)
-    qry_data = sorted(qry_data, key=lambda x: x[0])
-    min_timestamp = qry_data[0][0]
 
-    for i in range(len(qry_data)):
-        new_timestamp = (qry_data[i][0] - min_timestamp) / RESCALE_FACTOR
-        qry_data[i] = tuple([new_timestamp, qry_data[i][1], qry_data[i][2]])
-    print(f"Finished preprocessing in {time.time() - start:.2f}")
-    return workload_transactions, qry_data
+    return workload_transactions
 
 def idea1Parallel():
     pass
 
 def idea3Parallel(workload, mpl, nsize, iso_level):
+    workload_transactions = prepareTransactions(workload, nsize)
     worker_transactions = [[] for i in range(mpl)]
     worker = 0
-    for i in range(len(workload)):
-        worker_transactions[worker].append(workload[i])
+    for i in range(len(workload_transactions)):
+        worker_transactions[worker].append(workload_transactions[i])
         worker += 1
         worker %= mpl
     
     result_queue = mp.Queue()
     barrier = mp.Barrier(mpl + 1)
     processes = []
+    # 15 second delay for synchronization
     start_time = time.time() + 15
     for i in range(mpl):
         processes.append(mp.Process(target=workerIdea3, args=(worker_transactions[i], result_queue, iso_level, barrier, start_time, i)))
@@ -174,10 +184,12 @@ def idea3Parallel(workload, mpl, nsize, iso_level):
     for proc in processes:
         proc.start()
     time.sleep(2)
-    start = time.time()
     barrier.wait()
 
     print("Starting Simulation")
+    start = time.time()
+
+    # Collect data point from queue to avoid excessive RAM usage
     num_transactions = 0
     total_time = 0
     min_response = 10000000000000000
@@ -188,43 +200,45 @@ def idea3Parallel(workload, mpl, nsize, iso_level):
             response_time = result_queue.get_nowait()
             if response_time == -1:
                 num_terminate += 1
+                print(f"Received {num_terminate} signals")
             else:
                 num_transactions += 1
                 total_time += response_time
                 min_response = min(min_response, response_time)
                 max_response = max(max_response, response_time)
-                if num_transactions % 100000 == 0:
+                if num_transactions % 100 == 0:
                     print('----------------------------')
                     print(f"Time Elapsed: {time.time() - start}")
                     print(f"Total Txn Time: {total_time}")
                     print(f"Num Txns: {num_transactions}")
                     print(f"Avg Response Time: {total_time/num_transactions}")
                     print(f"Max Response Time: {max_response}")
+                    print(f"Min Response Time: {min_response}")
                     print('----------------------------')
         except queue.Empty:
             pass
     print(f"Simulation ended in {time.time() - start} seconds.")
-    # results_bank = []
-    # while True:
-    #    if result_queue.empty() == True:
-    #        break
-    #    results_bank.append(result_queue.get_nowait())
-    
-    # num_transactions = 0
-    # total_time = 0
-    # min_response = 10000000000000000
-    # max_response = -1
-    # for results in results_bank:
-    #     num_transactions += len(results["Response Time"])
-    #     for response_time in results["Response Time"]:
-    #         total_time += response_time
-    #         min_response = min(min_response, response_time)
-    #         max_response = max(max_response, response_time)
 
     print(f"Total time: {total_time}")
     print(f"Num Txns: {num_transactions}")
     print(f"Avg Response Time: {total_time/num_transactions}")
     print(f"Max Response Time: {max_response}")
+    print(f"Min Response Time: {min_response}")
+
+def cleanDatabase(createDBfname, dropDBfname):
+    conn = getConnection()
+    conn.set_session(autocommit=True)
+    with conn.cursor() as cur:
+        with open(dropDBfname, 'r') as f:
+            drop_tables = f.read()
+        cur.execute(drop_tables)
+        with open(createDBfname, 'r') as f:
+            create_tables = f.read()
+        cur.execute(create_tables)
+    conn.close()
+    time.sleep(5)
+    return
+
 
 def main():
     parser = argparse.ArgumentParser(description="Transaction simulator")
@@ -237,27 +251,30 @@ def main():
     assert args.mpl > 0
     assert args.nsize > 0
     assert args.isolation in [0, 1, 2, 3]
-    
+
     metadata_filename = f'./data/{args.conc}_concurrency/metadata.sql'
     query_filename = f'./queries/{args.conc}_concurrency/queries.txt'
     obs_filename = f'./data/{args.conc}_concurrency/observation_{args.conc}_concurrency.sql'
     semobs_filename = f'./data/{args.conc}_concurrency/semantic_observation_{args.conc}_concurrency.sql'
+    preprocessed_filename = f'./preprocessed.csv'
+
+    create_table_filename = f'./Docker/create.sql'
+    drop_table_filename = f'./Docker/drop.sql'
+
+    cleanDatabase(create_table_filename, drop_table_filename)
+    print("Database cleaned.")
 
     # Insert Metadata
     insertMetadata(metadata_filename)
+    print("Metadata inserted.")
 
     # Preprocess workload
-    workload, qry_workload = preprocessWorkload(query_filename, obs_filename, semobs_filename, args.nsize)
+    workload = preprocessWorkload(query_filename, obs_filename, semobs_filename, preprocessed_filename)
     
     # Spawn worker processes
     # Idea #1: have main process generate transactions, sleep for necessary amount of time before placing on queue
     # Idea #2: main process generates transactions, worker process sleeps if time does not match up
     # Idea #3: divide transactions amongst N workers, each worker sleeps (advantage: no input queue needed)
-    # transaction_queue = mp.Queue()
-    # result_queue = mp.Queue()
-    # processes = []
-    # for i in range(args.mpl):
-    #     processes.append(mp.Process(target=worker, args=(transaction_queue, result_queue, isolation_levels[args.isolation])))
     idea3Parallel(workload, args.mpl, args.nsize, isolation_levels[args.isolation])
 
 if __name__ == "__main__":
